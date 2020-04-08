bd8ab8504fb9abac36136810db78ca99eca986d4
Nicolás Vesga Rojas <48921123+nicolasvesga@users.noreply.github.com>
2020-04-08 08:13:51 -0500

    Add files via upload

---
+++ megaphono/includes/footerinterno.php
@@ -0,0 +1,37 @@
+<div class="reproductor">
+	<div class="reproductor__player">
+		<div class="reproductor__player--progress"></div>
+		<div class="reproductor__player--controls">
+			<div class="amplitude-play reproductor__player--controls-play play"></div>
+			<div class="amplitude-mute reproductor__player--controls-volumen sound">
+				<!-- <input type="range" class="amplitude-volume-slider"/> -->
+			</div>
+			<div class="reproductor__player--controls-info no_events" onclick="openModalLirycs()"></div>
+			<div class="reproductor__player--controls-bar">
+				<img src="img/player/soundBar.png" alt="soundBar">
+				<img src="img/player/soundBar.png" alt="soundBar" class="hidden">
+				<!-- <img src="img/player/soundbar.gif" alt="soundBar" class="hidden"> -->
+			</div>
+		</div>
+	</div>
+	<div class="reproductor__songs">
+		<div class="reproductor__songs--controls">
+			<div class="amplitude-prev reproductor__songs--controls-prev no_events"></div>
+			<div class="amplitude-next reproductor__songs--controls-next no_events"></div>
+		</div>
+		<div class="reproductor__songs--name">
+			Megaphono
+		</div>
+	</div>
+</div>
+
+<footer class="mainFooter">
+	<div class="mainFooter__content">
+		<p class="mainFooter--copy">© 2018 - All rights reserved.</p>
+		<div>
+			<a href="https://www.instagram.com/" target="_blank" class="mainFooter--social">
+				<img src="img/icons/iconInstagram.svg" alt="">
+			</a>
+		</div>
+	</div>
+</footer>
---
+++ megaphono/includes/headerinterno.php
@@ -0,0 +1,56 @@
+<?php
+
+	<div class="hamburger-menu">
+		<div class="bar">
+		</div>
+	</div>
+	<nav class="mainHeader__nav">
+		<ul>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#intro">
+					<span>Home</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#bio">
+					<span>Bio</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#media">
+					<span>Media</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#events">
+					<span>Events</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#gallery">
+					<span>Gallery</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#merch">
+					<span>Merch</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" href="#contact">
+					<span>Contact</span>
+				</a>
+			</li>
+			<li>
+				<a class="animatedHover animatedHover--verde" onclick="openModal('.modal__pdf')">
+					<span>EPK</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="mainHeader__traduction">
+		<a href="/index-en.php">
+			<span>ES</span>
+		</a>
+	</div>
+</header>
